7711902f9c777c289d1de0bf861547a8a0e887a8
Andres David Valdes Gonzalez <113150392+andresvaldesvg@users.noreply.github.com>
2024-10-04 21:46:32 +0930

    Added functionality in Windows.py
    Added more information and labels in the app.

--- widgets/windows.py
+++ widgets/windows.py
@@ -3,6 +3,8 @@ from tkinter import ttk
 
 def button_func():
     print('A button was pressed')
+def button_func2():
+    print('hello')
 
 #create a window
 window = tk.Tk()
@@ -21,8 +23,19 @@ text.pack()
 entry = ttk.Entry(master = window)
 entry.pack()
 
+# excercise text label
+ex_label = ttk.Label (master = window, text = 'My label')
+ex_label.pack()
+#ex_button = ttk.Button(master = window, text = 'Click here', command = button_func2)
+ex_button = ttk.Button(master = window, text = 'Click here', command = lambda: print('Hello World!'))
+ex_button.pack()
+
 #ttk button
 button = ttk.Button(master = window, text = 'A button', command = button_func)
 button.pack()
+
+
+
+
 #run 
 window.mainloop()
