c27ce65dd944a4abeee398e2663c1eac67616e5e
Andres David Valdes Gonzalez <113150392+andresvaldesvg@users.noreply.github.com>
2024-10-05 16:07:18 +0930

    ex1.py modified
    added more buttons and functionality

--- widgets/ex1.py
+++ widgets/ex1.py
@@ -5,9 +5,12 @@ def button_func():
     #entry content
     entry_text = entry.get()
 
+
     #update label
     #label.configure(text = 'More text')
     label['text'] = entry_text
+    entry['state'] = 'disabled'
+    #print(label.configure())
 
 #window
 window = tk.Tk()
@@ -23,6 +26,13 @@ entry.pack()
 button = ttk.Button(master = window, text = "Click here", command = button_func)
 button.pack()
 
+#ex
+def reset_func():
+    label['text'] = 'some text'
+    entry['state'] = 'enable'
+
+ex_button = ttk.Button(master = window, text = 'Restart', command = reset_func)
+ex_button.pack()
 
 #run
 window.mainloop()
